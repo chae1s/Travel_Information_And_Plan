94162b94b60291901bfafa61da78c83ef51765a9
chae1s <chae1zd.b@gmail.com>
2023-08-10 14:40:32 +0900

    [Feat] First Init

---
+++ src/main/java/com/example/Final_Project_9team/FinalProject9teamApplication.java
@@ -0,0 +1,13 @@
+package com.example.Final_Project_9team;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalProject9teamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalProject9teamApplication.class, args);
+	}
+
+}
