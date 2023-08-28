10ea87c2a57bbcc73bfbb1312c01fefb6d966d06
dustyooyu <somecalm@gmail.com>
2023-08-29 00:45:23 +0900

    chore : 주석 수정

--- src/main/java/com/example/Final_Project_9team/global/jwt/JwtTokenFilter.java
+++ src/main/java/com/example/Final_Project_9team/global/jwt/JwtTokenFilter.java
@@ -40,7 +40,7 @@ public class JwtTokenFilter extends OncePerRequestFilter {
 
         log.info("authHeader 확인: " + authHeader);
         // Header 검증
-        // 비어있거나, "Bearer "로 시작하는 경우
+        // 비어있지 않거나, "Bearer "로 시작하는 경우
         if (authHeader != null && authHeader.startsWith("Bearer ")) {
             String token = authHeader.split(" ")[1];
 
