50b77ac9003bab3855776b9314df78e2ed2a79f6
chae1s <chae1zd.b@gmail.com>
2023-09-04 16:24:52 +0900

    refactor : readSchedule에서 mates list 대신 user list로 수정
    MakeScheduleDetail.vue에서 user profile image 가져오기

--- src/main/java/com/example/Final_Project_9team/dto/ScheduleResponseDto.java
+++ src/main/java/com/example/Final_Project_9team/dto/ScheduleResponseDto.java
@@ -1,5 +1,6 @@
 package com.example.Final_Project_9team.dto;
 
+import com.example.Final_Project_9team.dto.user.UserResponseDto;
 import com.example.Final_Project_9team.entity.Schedule;
 import lombok.Data;
 import lombok.Getter;
@@ -21,6 +22,7 @@ public class ScheduleResponseDto {
     private Boolean display;
     private Integer period;
     private List<MatesResponseDto> matesResponses;
+    private List<UserResponseDto> userResponses;
 
     public static ScheduleResponseDto fromEntity(Schedule schedule) {
         ScheduleResponseDto dto = new ScheduleResponseDto();
@@ -50,6 +52,23 @@ public class ScheduleResponseDto {
         dto.setDisplay(schedule.getDisplay());
         dto.setPeriod(Period.between(schedule.getStartDate(), schedule.getEndDate()).getDays() + 1);
         dto.setMatesResponses(matesResponses);
+
+        return dto;
+    }
+
+    public static ScheduleResponseDto fromEntity(Schedule schedule, List<UserResponseDto> userResponses, Integer period) {
+        ScheduleResponseDto dto = new ScheduleResponseDto();
+        dto.setId(schedule.getId());
+        dto.setTitle(schedule.getTitle());
+        dto.setDescription(schedule.getDescription());
+        dto.setSido(schedule.getSido());
+        dto.setStartDate(schedule.getStartDate());
+        dto.setEndDate(schedule.getEndDate());
+        dto.setSumDistance(schedule.getSumDistance());
+        dto.setSumDuration(schedule.getSumDuration());
+        dto.setDisplay(schedule.getDisplay());
+        dto.setPeriod(period);
+        dto.setUserResponses(userResponses);
 
         return dto;
     }
