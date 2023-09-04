f459ed1ba754190f5cee6f9d26ba4c0cd0a965d0
dustyooyu <somecalm@gmail.com>
2023-09-04 21:48:35 +0900

    fix: 플러그인 추가 등

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,9 +3,14 @@ import App from './App.vue'
 import router from '@/router/router'
 import axios from 'axios'
 import '@/assets/css/fonts.css'
+import store from './store'
 
 const app = createApp(App)
+app
+    .use(router)
+    .use(store)
+    .mount('#app')
+
 
 app.config.globalProperties.axios = axios
-app.use(router)
-app.mount('#app')
+
