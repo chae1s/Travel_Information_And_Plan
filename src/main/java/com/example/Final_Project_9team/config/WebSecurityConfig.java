ffcf93d92f1a2449c6cff062bf153343ff2fd9d2
chae1s <chae1zd.b@gmail.com>
2023-09-05 18:05:59 +0900

    fix : 403 에러 해결 - spring security 코드 수정

--- src/main/java/com/example/Final_Project_9team/config/WebSecurityConfig.java
+++ src/main/java/com/example/Final_Project_9team/config/WebSecurityConfig.java
@@ -21,7 +21,7 @@ public class WebSecurityConfig {
     @Bean
     public WebSecurityCustomizer configure() {
         return (web) -> web.ignoring()
-                .requestMatchers("/static/**");
+                .requestMatchers("/static/**", "/js/**", "/css/**", "/img/**");
     }
 
     @Bean
@@ -30,11 +30,14 @@ public class WebSecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(authHttp -> authHttp
                         .requestMatchers(
+                                "/index.html",
+                                "/", "/error",
                                 "/users/register",
                                 "users/profile/**",
                                 "board-create",
                                 "schedules/chat/**", "static/**", "/js/**", "/chatting", "/error",
-                                "schedules/write/**"
+                                "schedules/write/**",
+                                "/schedules/**","/users/me/**"
                         )
                         .permitAll()
                         .requestMatchers(
