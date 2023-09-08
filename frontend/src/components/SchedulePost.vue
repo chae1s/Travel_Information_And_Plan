810fb14a009e10df2b6b53c2cd038989297a0b75
chae1s <chae1zd.b@gmail.com>
2023-09-09 04:05:46 +0900

    feat : 작성한 스케줄에 지도 추가, 동선, 마커 표시

--- frontend/src/components/SchedulePost.vue
+++ frontend/src/components/SchedulePost.vue
@@ -19,14 +19,14 @@
             <div class="schedule_item_header">
                 <span v-for="i in scheduleData.period" @click="changedDefaultDate(i)" :class="{'selected':checkedDefaultDate(i)}">Day {{ i }}</span>
             </div>
-            <div class="schedule_item_images">
+            <div class="schedule_item_images" v-if="defaultDate !== 0">
                 <div class="image_list">
                     <div v-for="item in tourRouteList[defaultDate - 1]" :key="item">
                         <v-img :src="item.firstImage" width="112px" height="112px" cover="true" class="rounded-lg"/>
                     </div>
                 </div>
             </div>
-            <div class="schedule_destination_list">
+            <div class="schedule_destination_list" v-if="defaultDate !== 0">
                 <ul>
                     <li v-for="(item, i) in tourRouteList[defaultDate - 1]" :key="item" class="schedule_item">
                         <div>
@@ -38,7 +38,11 @@
                             <div class="destination_information">{{ item.category }}</div>
                             <div v-if="i < itemPathList[defaultDate - 1].length" class="schedule_route_info">
                                 <div>이동시간 : {{ itemPathList[defaultDate - 1][i].duration }}</div>
-                                <div>이동시간 : {{ itemPathList[defaultDate - 1][i].distance }}</div>
+                                <div>이동거리 : {{ itemPathList[defaultDate - 1][i].distance }}</div>
+                            </div>
+                            <div v-if="i === itemPathList[defaultDate - 1].length" class="schedule_route_info">
+                                <div>총 이동시간 : {{ totalDuration[defaultDate - 1] }}</div>
+                                <div>총 이동거리 : {{ totalDistance[defaultDate - 1] }}</div>
                             </div>
                         </div>
                     </li>
@@ -68,9 +72,14 @@ export default {
                 isDisplay: '',
                 users: [{}]
             },
+            sidoCode: '',
             tourRouteList: [],
             itemPathList: [],
-            defaultDate: 1
+            defaultDate: 0,
+            totalDuration: [],
+            totalDistance: [],
+            zoom: 11,
+            polylineHex: ['#C4DFFF', '#FFE866', '#72D3B6', '#FFC7C2', '#B3B9FF']
         }
     },
     mounted() {
@@ -83,32 +92,94 @@ export default {
                 this.scheduleData.title = data.title
                 this.scheduleData.description = data.description
                 this.scheduleData.sido = locations[data.sido].name
+                this.sidoCode = data.sido
                 this.scheduleData.startDate = dayjs(data.startDate).format("YYYY.MM.DD")
                 this.scheduleData.endDate = dayjs(data.endDate).format("YYYY.MM.DD")
                 this.scheduleData.period = data.period
                 this.scheduleData.isDisplay = data.isDisplay
                 this.scheduleData.users = data.userResponses
-                console.log(data)
+
                 let scheduleItems = data.scheduleItemResponses
 
+                this.createMap(data.sido)
 
                 for (let i = 0; i < scheduleItems.length; i++) {
 
                     this.tourRouteList.push(scheduleItems[i].itemListResponses)
                     this.itemPathList.push(scheduleItems[i].itemPaths)
+                    this.totalDurationAndDistance(scheduleItems[i].itemPaths)
                 }
 
-                console.log(this.itemPathList)
 
             } catch (error) {
                 console.log(error)
             }
         },
+        createMap(sido) {
+            const script = document.createElement('script')
+            script.src = "https://openapi.map.naver.com/openapi/v3/maps.js?ncpClientId=y14z3lfrdm"
+
+            script.async = true
+            script.defer = true
+            document.head.appendChild(script)
+
+            if (this.scheduleData.sido === '0') this.zoom = 8
+
+            script.onload = () => {
+                new window.naver.maps.Map("map", {
+                    center: new window.naver.maps.LatLng(locations[sido].lat, locations[sido].lng),
+                    zoom: this.zoom
+                })
+            }
+        },
         changedDefaultDate(i) {
             this.defaultDate = i
+            this.createItemMarkerAndPolyline()
         },
         checkedDefaultDate(i) {
             return this.defaultDate === i
+        },
+        totalDurationAndDistance(itemPaths) {
+            let dateTotalDistance = 0
+            let dateTotalDuration = 0
+            for (let i = 0; i < itemPaths.length; i++) {
+                dateTotalDistance += itemPaths[i].distance
+                dateTotalDuration += itemPaths[i].duration
+            }
+
+            this.totalDuration.push(dateTotalDuration)
+            this.totalDistance.push(dateTotalDistance)
+        },
+        createItemMarkerAndPolyline() {
+            const map = new naver.maps.Map("map", {
+                center: new window.naver.maps.LatLng(locations[this.sidoCode].lat, locations[this.sidoCode].lng),
+                zoom: this.zoom
+            })
+
+            let polylinePath = []
+            this.tourRouteList[this.defaultDate - 1].forEach((destination) => {
+                polylinePath.push(new naver.maps.LatLng(parseFloat(destination.longitude), parseFloat(destination.latitude)))
+            })
+
+            const polyline = new naver.maps.Polyline({
+                map : map,
+                path: polylinePath,
+                strokeColor : this.polylineHex[this.defaultDate - 1],
+                strokeOpacity: 0.8,
+                strokeWeight: 6
+            })
+
+
+            this.tourRouteList[this.defaultDate - 1].forEach((destination, index) => {
+                const marker = new naver.maps.Marker({
+                    position: new naver.maps.LatLng(parseFloat(destination.longitude), parseFloat(destination.latitude)),
+                    map: map,
+                    icon: {
+                        url: require('@/assets/images/icons/day' + (this.defaultDate) + '/course_pin_' + (index + 1) + '.png'),
+                        scaledSize: new naver.maps.Size(40, 40)
+                    }
+                })
+            })
         },
     }
 }
