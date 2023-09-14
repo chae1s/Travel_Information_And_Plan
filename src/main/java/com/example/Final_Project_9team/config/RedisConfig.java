33046d6719e8b69b35bdcf117eac7f4f47cf0ce9
chae1s <chae1zd.b@gmail.com>
2023-09-14 20:05:54 +0900

    refactor : Docker의 레디스를 사용하기 위해 설정 변경

--- src/main/java/com/example/Final_Project_9team/config/RedisConfig.java
+++ src/main/java/com/example/Final_Project_9team/config/RedisConfig.java
@@ -4,6 +4,7 @@ import com.example.Final_Project_9team.dto.SchedulePathDto;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
@@ -15,7 +16,15 @@ public class RedisConfig {
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory();
+
+        RedisStandaloneConfiguration redisConfig = new RedisStandaloneConfiguration();
+
+        redisConfig.setHostName("3.35.51.90");
+        redisConfig.setPort(6379);
+
+        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(redisConfig);
+        lettuceConnectionFactory.afterPropertiesSet();
+        return lettuceConnectionFactory;
     }
 
     @Bean
